40c2f3f7b6c59ddb5299b6f5a1c5afdd6274cdd2
Geoffrey Singer <geoffreysinger@Geoffreys-MacBook-Pro.local>
2024-11-05 16:54:45 -0500

    Update game template and structure

--- pygame_input_demo/main.py
+++ pygame_input_demo/main.py
@@ -1,1 +1,31 @@
- 
+import asyncio
+import pygame
+
+pygame.init()
+pygame.display.set_mode((320, 240))
+clock = pygame.time.Clock()
+
+async def main():
+    count = 60
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                break
+
+        print(f"{count}: Hello from Pygame")
+        pygame.display.update()
+        await asyncio.sleep(0)
+
+        if not count:
+            running = False
+            break
+        
+        count -= 1
+        clock.tick(60)
+    
+    pygame.quit()
+
+asyncio.run(main()) 
